6a39ca0137e85e8d935e9dc08220c2b997ee27a1
user1000 <user1000@2024-003-001.local>
2025-06-05 13:43:34 +0200

    upd 16

--- netfox/FastRequest/FastRequestResult/FastRequestResultView.swift
+++ netfox/FastRequest/FastRequestResult/FastRequestResultView.swift
@@ -91,17 +91,20 @@ public struct FastRequestResultView: View {
         ZStack {
             ZStack {
                 VStack() {
-                    Text(isProtect ? String(format: model?.scn?.title_compl ?? "", localizeText(forKey: .subsOn)) : String(format: model?.scn?.title_compl ?? "", localizeText(forKey: .subsDis)))
-                        .font(.system(size: Constants.smallScreen ? 20 : 33, weight: .bold, design: .default))
-                        .foregroundStyle(.black)
-                        .padding(.top, Constants.smallScreen ? 5 : 50)
+                    VStack {
+                        Text(isProtect ? String(format: model?.scn?.title_compl ?? "", localizeText(forKey: .subsOn)) : String(format: model?.scn?.title_compl ?? "", localizeText(forKey: .subsDis)))
+                            .font(.system(size: Constants.smallScreen ? 20 : 33, weight: .bold, design: .default))
+                            .foregroundStyle(.black)
+                            .padding(.top, Constants.smallScreen ? 5 : 50)
+                        
+                        Text(isProtect ? model?.scn?.subtitle_compl ?? "" : model?.scn?.subtitle_unp ?? "")
+                            .font(.system(size: Constants.smallScreen ? 14 : 16, weight: .medium, design: .default))
+                            .lineLimit(2)
+                            .foregroundStyle(Color(red: 156/255, green: 156/255, blue: 156/255))
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                    }
                     
-                    Text(isProtect ? model?.scn?.subtitle_compl ?? "" : model?.scn?.subtitle_unp ?? "")
-                        .font(.system(size: Constants.smallScreen ? 14 : 16, weight: .medium, design: .default))
-                        .lineLimit(2)
-                        .foregroundStyle(Color(red: 156/255, green: 156/255, blue: 156/255))
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal)
                     
                     ZStack {
                         if isProtect {
